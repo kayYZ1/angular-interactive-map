35af500d2b49e74dfd108dea18877b3017b28236
kayYZ1 <overek7133@gmail.com>
2024-06-16 10:07:33 +0200

    minor

--- src/app/features/components/sidebar/sidebar-list/sidebar-list.component.ts
+++ src/app/features/components/sidebar/sidebar-list/sidebar-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { SearchFilterPipe } from '../../../../../core/pipes/search-filter.pipe';
-import { CriteriaFilterPipe } from '../../../../../core/pipes/criteria-filter.pipe';
+import { Component, OnInit, inject } from '@angular/core';
+import { SearchFilterPipe } from '../../../../core/pipes/search-filter.pipe';
+import { CriteriaFilterPipe } from '../../../../core/pipes/criteria-filter.pipe';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { Store } from '@ngrx/store';
 import { AsyncPipe } from '@angular/common';
-import { IFilters, IObject } from '../../../../../core/ts/interfaces';
-import { addToTrip } from '../../../../../core/store/trip/trip.actions';
-import { selectPlaces } from '../../../../../core/store/trip/trip.selectors';
-import { selectFilters } from '../../../../../core/store/filters/filters.selectors';
-import { Objects } from '../../../../../core/data/objects';
+import { IFilters, IObject } from '../../../../core/ts/interfaces';
+import { addToTrip } from '../../../../core/store/trip/trip.actions';
+import { selectPlaces } from '../../../../core/store/trip/trip.selectors';
+import { selectFilters } from '../../../../core/store/filters/filters.selectors';
+import { Objects } from '../../../../core/data/objects';
 
 @Component({
   selector: 'app-sidebar-list',
@@ -35,8 +35,8 @@ export class SidebarListComponent implements OnInit {
   page = 1;
 
   ngOnInit(): void {
-    this.places$.subscribe((data) => this.places = data);
-    this.filters$.subscribe((data) => this.filters = data);
+    this.places$.subscribe((data) => (this.places = data));
+    this.filters$.subscribe((data) => (this.filters = data));
   }
 
   onClick(object: IObject) {
--- src/app/features/components/sidebar/sidebar.component.ts
+++ src/app/features/components/sidebar/sidebar.component.ts
@@ -3,12 +3,17 @@ import { Component, Input } from '@angular/core';
 import { SidebarFiltersComponent } from './sidebar-filters/sidebar-filters.component';
 import { SidebarTripComponent } from './sidebar-trip/sidebar-trip.component';
 import { SidebarSaveComponent } from './sidebar-save/sidebar-save.component';
-import { SidebarListComponent } from './sidebar-filters/sidebar-list/sidebar-list.component';
+import { SidebarListComponent } from './sidebar-list/sidebar-list.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarFiltersComponent, SidebarTripComponent, SidebarSaveComponent, SidebarListComponent],
+  imports: [
+    SidebarFiltersComponent,
+    SidebarTripComponent,
+    SidebarSaveComponent,
+    SidebarListComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
