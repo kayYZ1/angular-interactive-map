be67b7b074638ef980d7e18771d3dfac2b295d4d
kayYZ1 <106306274+kayYZ1@users.noreply.github.com>
2024-08-01 11:41:31 +0000

    Deploying to prod from @ kayYZ1/angular-interactive-map@a1ff29db8ef7632b48b842c949e5ce413ef4468c 🚀

--- src/app/core/store/filters/filters.selectors.ts
+++
@@ -1,5 +0,0 @@
-import { createFeatureSelector } from "@ngrx/store";
-
-import { IFilters } from "@/shared/ts/interfaces";
-
-export const selectFilters = createFeatureSelector<IFilters>('filters');
